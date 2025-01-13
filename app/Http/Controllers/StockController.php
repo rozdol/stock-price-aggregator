68c69ff38d34c8e4d79c6e19ed018f4ea5eee0cc
Alex Titoff <rozdol@gmail.com>
2025-01-13 15:22:39 +0200

    Add API

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -3,17 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
-use App\Models\Stock;
 
 class StockController extends Controller
 {
-    public function getSymblos()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
     {
-        $stock = Stock::all();
-        if (!$stock) {
-            return response()->json(['message' => 'No data found'], 404);
-        }
-        return response()->json($stock);
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
