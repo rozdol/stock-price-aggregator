74ca3a29b599117366cbccbfdb34a82b53806490
Alex Titoff <rozdol@gmail.com>
2025-01-14 13:24:50 +0200

    fetching bulk prices

--- app/Console/Commands/FetchStockPrices.php
+++ app/Console/Commands/FetchStockPrices.php
@@ -31,6 +31,47 @@ class FetchStockPrices extends Command
      */
     protected $description = 'Fetch real-time stock prices and store them in the database.';
 
+    private function storePrice($symbol, $price)
+    {
+        $stockId = Stock::where('symbol', $symbol)->value('id');
+        if ($stockId) {
+            // Store in DB
+            StockPrice::create([
+                'stock_id' => $stockId,
+                'price' => $price,
+                'retrieved_at' => now(),
+            ]);
+
+            // Store data in Cache
+            $data = Cache::get("stock_bkp:{$symbol}");
+            if ($data) {
+                // dump($data);
+                $previousPrice = $data['price'];
+                $change_pct = (($price - $previousPrice) / $previousPrice) * 100;
+                $retrieved_at_prev = Carbon::parse($data['retrieved_at']);
+                $secondsDifference = $retrieved_at_prev->diffInSeconds(now());
+            } else {
+                $change_pct = 0;
+                $previousPrice = 0;
+                $secondsDifference = 0;
+            }
+            // Update cache
+            $cached_data = [
+                'price' => $price,
+                'change_pct' => $change_pct,
+                'retrieved_at' => now()->toDateTimeString(),
+            ];
+            Log::info("{$symbol} price:$price and $secondsDifference seconds from previous price=$previousPrice");
+
+            // dump($cached_data);
+
+            Cache::put("stock:{$symbol}", $cached_data, 60); // keep only 60 seconds fresh prices
+            Cache::put("stock_bkp:{$symbol}", $cached_data, 3600); // keep data tocalculate change_pct of the refresh period is more than 60 seconds.
+            $this->info("Updated cache for $symbol at price=$price with change_pct=$change_pct");
+        } else {
+            $this->error("No $symbol in the database.");
+        }
+    }
     /**
      * Execute the console command.
      */
@@ -38,68 +79,78 @@ class FetchStockPrices extends Command
     {
         $stocks = Stock::all();
         $apiKey = env("ALPHA_VANTAGE_API_KEY", "demo");
-        // dd($apiKey);
+        $isPremium = env("ALPHA_VANTAGE_PEMIUM", false);
 
-        foreach ($stocks as $stock) {
+        // Get prices for all symbols in one query if the the account is premium
+        if ($isPremium) {
+            Log::info("Premium");
+            $this->info("Premium");
+            $symbols = Stock::pluck('symbol')->toArray();
+            $commaSeparatedSymbols = implode(',', $symbols);
             $response = Http::get("https://www.alphavantage.co/query", [
-                'function' => 'GLOBAL_QUOTE',
-                'symbol' => $stock->symbol,
+                'function' => 'REALTIME_BULK_QUOTES',
+                'symbol' => $commaSeparatedSymbols,
                 'apikey' => $apiKey,
             ]);
-
-            $this->info('Quering ' . $stock->symbol);
-
             if ($response->ok()) {
-                // dd($response);
-                $data = $response->json()['Global Quote'] ?? null;
-                // dd($data);
-                if ($data) {
-                    // dd($data);
-                    $price = (float) $data['05. price'];
-                    $this->info($stock->symbol . ': ' . $price);
+                // print_r($commaSeparatedSymbols);
+                $prices = $response->json()['data'] ?? null;
+                if ($prices) {
+                    // dd($response->json()['data']);
+                    foreach ($prices as $item) {
+                        $symbol = $item['symbol'];
+                        $price = (float) $item['close'];
 
-                    // Store in DB
-                    StockPrice::create([
-                        'stock_id' => $stock->id,
-                        'price' => $price,
-                        'retrieved_at' => now(),
-                    ]);
+                        // Store in DB
+                        $this->storePrice($symbol, $price);
 
-                    // 
-                    $data = Cache::get("stock_bkp:{$stock->symbol}");
-                    if ($data) {
-                        // dump($data);
-                        $previousPrice = $data['price'];
-                        $change_pct = (($price - $previousPrice) / $previousPrice) * 100;
-                        $retrieved_at_prev = Carbon::parse($data['retrieved_at']);
-                        $secondsDifference = $retrieved_at_prev->diffInSeconds(now());
-                    } else {
-                        $change_pct = 0;
-                        $previousPrice = 0;
-                        $secondsDifference = 0;
+                        echo ("Symbol: $symbol \n");
+                        $data = $response->json()['Global Quote'] ?? null;
                     }
-                    // Update cache
-                    $cached_data = [
-                        'price' => $price,
-                        'change_pct' => $change_pct,
-                        'retrieved_at' => now()->toDateTimeString(),
-                    ];
-                    Log::info("{$stock->symbol} price:$price and $secondsDifference seconds from previous price=$previousPrice");
-
-                    // dump($cached_data);
-
-                    Cache::put("stock:{$stock->symbol}", $cached_data, 60); // keep only 60 seconds fresh prices
-                    Cache::put("stock_bkp:{$stock->symbol}", $cached_data, 3600); // keep data tocalculate change_pct of the refresh period is more than 60 seconds.
-                    $this->info("Updated cache for $stock->symbol at price=$price with change_pct=$change_pct");
                 } else {
-                    $this->error('ERROR. Chaeck API KEY');
+                    $this->error('ERROR. Check API KEY');
                 }
             } else {
-                $this->info('No data for  ' . $stock->symbol);
+                $this->info('No data for  ' . $commaSeparatedSymbols);
             }
-        }
+        } else {
+            // Get prices for symbols one by one if the the account is not premium
+            Log::info("NOT Premium");
+            $this->info("NOT Premium");
+            foreach ($stocks as $stock) {
+                $response = Http::get("https://www.alphavantage.co/query", [
+                    'function' => 'GLOBAL_QUOTE',
+                    'symbol' => $stock->symbol,
+                    'apikey' => $apiKey,
+                ]);
 
-        $this->info('Stock prices updated successfully.');
-        Log::info('Stock prices updated successfully.');
+                $this->info('Quering ' . $stock->symbol);
+
+                if ($response->ok()) {
+                    // dd($response);
+                    $data = $response->json()['Global Quote'] ?? null;
+                    // dd($data);
+                    if ($data) {
+                        // dd($data);
+                        $price = (float) $data['05. price'];
+                        $this->info($stock->symbol . ': ' . $price);
+
+                        // Store in DB
+                        $this->storePrice($stock->symbol, $price);
+
+
+                        // 
+
+                    } else {
+                        $this->error('ERROR. Check API KEY');
+                    }
+                } else {
+                    $this->info('No data for  ' . $stock->symbol);
+                }
+            }
+
+            $this->info('Stock prices updated successfully.');
+            Log::info('Stock prices updated successfully.');
+        }
     }
 }
